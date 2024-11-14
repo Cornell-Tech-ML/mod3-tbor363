7df39841b6fbbaf8a5c8217d0abc3d9029ddcaeb
Tamara Bo Rabin <tr363@cornell.edu>
2024-11-14 15:40:53 -0500

    edit map and zip

--- minitorch/cuda_ops.py
+++ minitorch/cuda_ops.py
@@ -174,11 +174,13 @@ def tensor_map(
         in_index = cuda.local.array(MAX_DIMS, numba.int32)
         i = cuda.blockIdx.x * cuda.blockDim.x + cuda.threadIdx.x
         # TODO: Implement for Task 3.3.
-        to_index(i, out_shape, out_index)
-        broadcast_index(out_index, out_shape, in_shape, in_index)
-        o = index_to_position(out_index, out_strides)
-        j = index_to_position(in_index, in_strides)
-        out[o] = fn(in_storage[j])
+
+        if i < out_size:
+            to_index(i, out_shape, out_index)
+            broadcast_index(out_index, out_shape, in_shape, in_index)
+            o = index_to_position(out_index, out_strides)
+            j = index_to_position(in_index, in_strides)
+            out[o] = fn(in_storage[j])
 
     return cuda.jit()(_map)  # type: ignore
 
@@ -221,13 +223,14 @@ def tensor_zip(
         i = cuda.blockIdx.x * cuda.blockDim.x + cuda.threadIdx.x
 
         # TODO: Implement for Task 3.3.
-        to_index(i, out_shape, out_index)
-        o = index_to_position(out_index, out_strides)
-        broadcast_index(out_index, out_shape, a_shape, a_index)
-        j = index_to_position(a_index, a_strides)
-        broadcast_idnex(out_index, out_shape, b_shape, b_index)
-        k = index_to_position(b_index, b_strides)
-        out[o] = fn(a_storage[j], b_storage[k])
+        if i < out_size:
+            to_index(i, out_shape, out_index)
+            o = index_to_position(out_index, out_strides)
+            broadcast_index(out_index, out_shape, a_shape, a_index)
+            j = index_to_position(a_index, a_strides)
+            broadcast_index(out_index, out_shape, b_shape, b_index)
+            k = index_to_position(b_index, b_strides)
+            out[o] = fn(a_storage[j], b_storage[k])
 
     return cuda.jit()(_zip)  # type: ignore
 
